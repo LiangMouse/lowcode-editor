a6139fcc5706df9fc151dd053c2cc5073eaa2260
liangshuang <1979139861@qq.com>
2024-11-30 15:56:17 +0800

    引入allotement实现响应式拖动pane

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,3 +1,19 @@
-export default function LowcodeEditor() {
-  return <div>Lowcode Editor</div>;
+import { Allotment } from "allotment"; //实现可拖动改变大小的pane
+import "allotment/dist/style.css";
+
+export default function ReactPlayground() {
+  return (
+    <div className="h-[100vh] flex flex-col">
+      <div className="">Header</div>
+      <Allotment>
+        <Allotment.Pane preferredSize={240} maxSize={300} minSize={200}>
+          Material
+        </Allotment.Pane>
+        <Allotment.Pane>EditArea</Allotment.Pane>
+        <Allotment.Pane preferredSize={300} maxSize={500} minSize={300}>
+          Seeting
+        </Allotment.Pane>
+      </Allotment>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // 意图是将以上文件都作为内容文件进行编译提取className
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
